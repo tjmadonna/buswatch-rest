0923e1752b57f5dbb1c6571a28f45b951b50dda8
Tyler Madonna <developer@madonnaapps.com>
2020-05-02 16:53:49 -0400

    Removed stops route and model

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
 // Setup routers
-const stopsRouter = require('./routes/stops')
-app.use('/stops', stopsRouter)
 
 // Start listening
 app.listen(process.env.PORT, () => console.log('Server started'))
